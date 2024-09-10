49d0242c9eeb62595be7ef99fa93279c9a3d3bd1
D-Malex <3matias.sm@gmail.com>
2024-09-10 14:16:32 -0300

    Ejercicio2

--- Clases/Clase7.R
+++ Clases/Clase7.R
@@ -8,11 +8,35 @@ library(readr)
 library(dplyr)
 library(expss)
 
-View(
-    jiquad_a<- qchisq(p=1-0.900, df=5)
-)
-
+View(jiquad_a<- qchisq(p=1-0.900, df=5))
 # Estimacion del desvio estandar poblacional (Signma)
 # desvio estandar muestral (ese)s
 # Varianza poblacional: desvio al cuadrado
 # ????
+
+# Para calcular la VARIANZA podemos calcular el s2(s cuadrado) por medio de la funcion 'var()' que nos devuelve la varianza de un vector de datos.
+
+
+#__________________________________________________________________________
+# Los siguientes son los pesos en decagramos de 10 paquetes de semillas de pasto distribuidas por la empresa Amanecer:
+muestra <- c(46.4, 46.1, 45.8, 47.0, 46.1, 45.9, 45.8, 46.9, 45.2, 46.0)
+# Proporcionar un intervalo de confianza del 95% para la viarianza de todos los pesos de este tipo de paquetes de semillas de pasto distribuidas por la empresa. La probabildiad se distribuye normal.
+
+#DATOS
+porcentaje = 0.95
+alpha   <- (1-porcentaje) 
+muestra <- c(46.4, 46.1, 45.8, 47.0, 46.1, 45.9, 45.8, 46.9, 45.2, 46.0)
+
+#VALORES
+n  = length(muestra)
+s2 = var(muestra)
+x2 = (n-1)
+
+# RESPUESTA
+qchisq(alpha,x2,lower.tail=F)
+qchisq(alpha,x2,lower.tail=T)
+qchisq(porcentaje,x2,lower.tail=T)
+
+qchisq(0.05,9,lower.tail=F)
+qchisq(0.05,9,lower.tail=T)
+qchisq(0.95,9,lower.tail=T)
