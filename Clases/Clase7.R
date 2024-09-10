61f7bc4f8882ceaf26d2a9f4185cd564237881a2
D-Malex <3matias.sm@gmail.com>
2024-09-10 13:50:40 -0300

    Comienzo de la clase 10/09/24

---
+++ Clases/Clase7.R
@@ -0,0 +1,18 @@
+# IMPORTS & DECLARES
+install.packages("languageserver")
+install.packages("dplyr")
+install.packages("readr")
+install.packages("expss")
+library(languageserver)
+library(readr)
+library(dplyr)
+library(expss)
+
+View(
+    jiquad_a<- qchisq(p=1-0.900, df=5)
+)
+
+# Estimacion del desvio estandar poblacional (Signma)
+# desvio estandar muestral (ese)s
+# Varianza poblacional: desvio al cuadrado
+# ????
